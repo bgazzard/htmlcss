2590c2b41f06e9eba962aeecf37ec756570f5d20
bgazzard <bgazzard@users.noreply.github.com>
2021-06-14 09:18:08 +0100

    add content for test

---
+++ stories/filedownload.stories.js
@@ -0,0 +1,12 @@
+import './assets/spinner.css';
+
+import './filedownload.css';
+
+const html = require('./filedownload.html');
+
+export default {
+  title: 'File Download',
+};
+
+export const FileDownload = () => html;
+
